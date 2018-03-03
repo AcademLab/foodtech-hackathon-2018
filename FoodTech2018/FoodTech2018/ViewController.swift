672c1dca44566b40f409d20a4ed5fcde3d751667
Amin Benarieb <amin.benarieb@gmail.com>
2018-03-03 21:42:14 +0300

    Inital commit

---
+++ FoodTech2018/FoodTech2018/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FoodTech2018
+//
+//  Created by Amin Benarieb on 03/03/2018.
+//  Copyright © 2018 Amin Benarieb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
